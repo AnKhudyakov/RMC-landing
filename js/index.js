5297bb57d65a8fc8d1cb71dd0a5a8047e3b7c7f8
AnKhudyakov <andrew_job@list.ru>
2023-05-16 16:59:21 +0400

    feat: add slider

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  let x = document.getElementsByClassName("services__item_slider");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
