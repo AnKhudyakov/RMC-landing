6fbee69b759c0cf7ce0f3e990aa3046239a1a788
AnKhudyakov <andrew_job@list.ru>
2023-05-17 13:33:35 +0400

    feat: add modal-window

--- js/index.js
+++ js/index.js
@@ -20,12 +20,41 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
 }
 
-
 //Burger Menu
 const burgerMenuButton = document.querySelector("#burger-button");
 const navigationMenu = document.querySelector(".header__burger-menu");
+const burgerLink = document.querySelectorAll(".menu__link_burger");
 
 burgerMenuButton.addEventListener("click", () => {
   burgerMenuButton.classList.toggle("open");
   navigationMenu.classList.toggle("open");
+});
+
+burgerLink.forEach(function (link) {
+  link.addEventListener("click", () => {
+    burgerMenuButton.classList.toggle("open");
+    navigationMenu.classList.toggle("open");
+  });
+});
+
+//Modal Window
+const modalButtons = document.querySelectorAll("#modal-button");
+const modalWindow = document.querySelector(".page__modal");
+const closeButton = document.querySelector("#close-button");
+const submitForms = document.querySelectorAll("#submit");
+submitForms.forEach(function (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.reset();
+  });
+});
+
+modalButtons.forEach(function (button) {
+  button.addEventListener("click", () => {
+    modalWindow.style.display = "block";
+  });
+});
+
+closeButton.addEventListener("click", () => {
+  modalWindow.style.display = "none";
 });
